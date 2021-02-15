11d78a58d078e762d6114df7dcc4314f987aca49
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2021-02-15 11:57:17 +0900

    er

--- chapter04/pointcloud_usage/src/cloud_save.cpp
+++ chapter04/pointcloud_usage/src/cloud_save.cpp
@@ -1,5 +1,6 @@
 #include <pointcloud_usage/create_cloud.h>
 #include <pointcloud_usage/child_cloud.h>
+#include <pcl/point_types_conversion.h>
 
 static int cout = 0;
 ros::Publisher pub;
@@ -17,9 +18,11 @@ void point_callback(const sensor_msgs::PointCloud2ConstPtr &cloud_msg)
     PointCloud_class::segment(inlier, cloud, coefficient);
     PointCloud_class::extract(inlier, cloud, segment, without_segment);
     pcl::PointCloud<pcl::PointXYZI> inten_cloud;
-    pcl::copyPointCloud(without_segment, inten_cloud);
-    PointCloud_class::save_pcd(without_segment, "/home/tsuchidashinya/yes_1.pcd");
-    Point_and_ROS::convert_1_to_2(without_segment, sensr);
+    //pcl::copyPointCloud(without_segment, inten_cloud);
+    pcl::PointCloudXYZRGBtoXYZI(without_segment, inten_cloud);
+    PointCloud_class::save_pcd(inten_cloud, "/home/tsuchidashinya/yes_1.pcd");
+    Point_and_ROS::convert_1_to_2(inten_cloud, sensr);
+    sensr.header.frame_id = cloud_msg->header.frame_id;
     pub.publish(sensr);
     
 
